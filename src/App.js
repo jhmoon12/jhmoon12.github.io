8376d3f0e068c365a0dc23e0bbcaa0753ee22452
jhmoon12 <mjh9461@gmail.com>
2022-04-10 03:15:29 +0900

    update Params

--- src/App.js
+++ src/App.js
@@ -39,8 +39,8 @@ function App() {
             </div>
           </div>  
       </Route>
-      <Route path="/detail">
-        <Detail />
+      <Route path="/detail/:id">
+        <Detail shoes={shoes}/>
       </Route>
     </div>
   );
--- src/Detail.js
+++ src/Detail.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom'
+import { useHistory, useParams } from 'react-router-dom'
 
-function Detail(){
+function Detail({shoes}){
 
     let history = useHistory();//history 오브젝트 생성
+    let { id } = useParams();
+    let findId = shoes.find((shoe)=>{
+        return shoe.id == id
+    });
 
     return(
       <div className="container">
@@ -12,9 +16,9 @@ function Detail(){
           <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
         </div>
         <div className="col-md-6 mt-4">
-          <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>120000원</p>
+          <h4 className="pt-5">{findId.title}</h4>
+          <p>{findId.content}</p>
+          <p>{findId.price}</p>
           <button className="btn btn-danger">주문하기</button> 
           <button className="btn back-button" onClick={()=>{history.goBack();}}>뒤로가기</button> 
         </div>
