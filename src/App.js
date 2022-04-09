57688b3d38b0b75c99a11549952b45fb5a5f924c
jhmoon12 <mjh9461@gmail.com>
2022-04-09 23:49:19 +0900

    updated Route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 import { useState } from 'react';
 import Data from './data'
 import Col from './Col'
+import Detail from './Detail'
+import { Link, Route, Switch } from 'react-router-dom'
+
 
 
 function App() {
@@ -14,30 +17,35 @@ function App() {
     <div className="App">
       <Navbar bg="light" variant="light">
         <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand >Navbar</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link ><Link to="/">Home</Link></Nav.Link>
+          <Nav.Link ><Link to="/Detail">Detail</Link></Nav.Link>
+          <Nav.Link >Pricing</Nav.Link>
         </Nav>
         </Container>
       </Navbar>
-      <div className="bg-light p-5 rounded-lg m-3">	
-            <h1 className="display-4">20% sale</h1>	
-            <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>	
-            <hr className="my-4" />	
-            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>	
-            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>	
-      </div>
-      <div className="container">
-        <div className="row">
-           <Col shoes={shoes}/>
-        </div>
-      </div>
-      
-  
+      <Route exact path="/">{/* 속성 */}
+          <div className="bg-light p-5 rounded-lg m-3">	
+                <h1 className="display-4">20% sale</h1>	
+                <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>	
+                <hr className="my-4" />	
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>	
+                <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>	
+          </div>
+          <div className="container">
+            <div className="row">
+              <Col shoes={shoes}/>
+            </div>
+          </div>  
+      </Route>
+      <Route path="/detail">
+        <Detail />
+      </Route>
     </div>
   );
 }
+
+
 
 export default App;
